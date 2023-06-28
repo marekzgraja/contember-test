3bdf80bc2c48f4338249b8e9fce52e5340cdd937
Marek Zgraja <marek.zgraja@gmail.com>
2023-06-28 10:55:16 +0200

    removed chakra

--- admin/index.tsx
+++ admin/index.tsx
@@ -13,28 +13,26 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 runReactApp(
-  <ChakraProvider>
-    <DirectivesProvider>
-      <LayoutSlotsProvider>
-        <ApplicationEntrypoint
-          basePath={import.meta.env.BASE_URL}
-          apiBaseUrl={import.meta.env.VITE_CONTEMBER_ADMIN_API_BASE_URL}
-          sessionToken={import.meta.env.VITE_CONTEMBER_ADMIN_SESSION_TOKEN}
-          project={import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME}
-          stage="live"
-          envVariables={{
-            WEB_URL: import.meta.env.VITE_CONTEMBER_ADMIN_WEB_URL,
-          }}
-          children={
-            <Pages
-              children={import.meta.glob<PageModule>("./pages/**/*.tsx")}
-              layout={Layout}
-            />
-          }
-        />
-      </LayoutSlotsProvider>
-    </DirectivesProvider>
-  </ChakraProvider>,
+  <DirectivesProvider>
+    <LayoutSlotsProvider>
+      <ApplicationEntrypoint
+        basePath={import.meta.env.BASE_URL}
+        apiBaseUrl={import.meta.env.VITE_CONTEMBER_ADMIN_API_BASE_URL}
+        sessionToken={import.meta.env.VITE_CONTEMBER_ADMIN_SESSION_TOKEN}
+        project={import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME}
+        stage="live"
+        envVariables={{
+          WEB_URL: import.meta.env.VITE_CONTEMBER_ADMIN_WEB_URL,
+        }}
+        children={
+          <Pages
+            children={import.meta.glob<PageModule>("./pages/**/*.tsx")}
+            layout={Layout}
+          />
+        }
+      />
+    </LayoutSlotsProvider>
+  </DirectivesProvider>,
   null,
   (dom, react, onRecoverableError) =>
     createRoot(dom, { onRecoverableError }).render(react)
